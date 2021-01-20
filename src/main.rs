2a6c8b3bb068cf2b0d567bfda353e4bb1087c29c
stew3254 <rtstewy@gmail.com>
2021-01-20 15:47:00 -0500

    Add very basic checking to language and allow reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,64 @@
-use std::fmt;
+use anyhow::{anyhow, Error};
+use std::fs;
 
 mod parser;
 
-#[derive(thiserror::Error, Debug)]
-enum Error {
-  #[error("invalid program (expected {expected:?}, go {found:?})")]
-  InvalidProgram {
-    expected: String,
-    found: String,
-  }
+#[derive(Debug)]
+enum Direction {
+  Forward,
+  Backward,
+}
+
+#[derive(Debug)]
+struct Loop {
+  instr_ptr: i32,
+  jump_dir: Direction,
 }
 
 #[derive(Debug)]
 struct ProgramStack {
   program: String,
   inst_ptr: i32,
-  data: Vec<i32>,
+  data: Vec<i8>,
   data_ptr: i32,
+  stack: Vec<Loop>,
 }
 
 impl ProgramStack {
   pub fn from(program: String) -> Result<ProgramStack, Error> {
     match program.len() {
-      0 => Err(
-        Error::InvalidProgram {
-          expected: String::from("length to be greater than 0"),
-          found: String::new()
-        }
-      ),
+      0 => Err(anyhow::anyhow!("Program cannot be of length 0")),
       _ => {
-        if let Ok(_) = parser::parse(&program) {
-          Ok(
-            ProgramStack {
-              program,
-              inst_ptr: 0,
-              data: vec![],
-              data_ptr: 0
-            }
-          )
+        // See if the program is parsable
+        if let Err(p) = parser::parse(&program) {
+          return Err(p);
         }
+
+        Ok(ProgramStack {
+          program,
+          inst_ptr: 0,
+          data: Vec::with_capacity(32),
+          data_ptr: 0,
+          stack: Vec::new(),
+        })
       }
     }
   }
+
+  pub fn from_file(filename: String) -> Result<ProgramStack, Error> {
+    let program = match fs::read_to_string(filename) {
+      Ok(s) => s,
+      Err(e) => return Err(anyhow!(e)),
+    };
+    ProgramStack::from(program)
+  }
+  
+  pub fn run(&self) -> Result<(), Error> {
+    Ok(())
+  }
+
 }
 
 fn main() {
-  ProgramStack::from(String::from(""));
+  println!("{:?}", ProgramStack::from_file(String::from("test.bf")));
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,24 +1,36 @@
-use crate::Error;
-use crate::Error::InvalidProgram;
+use anyhow::{anyhow, Error};
 
-pub fn parse(p: &String) -> Result<(), Error>{
+// Quickly parse program lint before trying to run
+pub fn parse(p: &String) -> Result<(), Error> {
+  let mut index_stack: Vec<char> = Vec::new();
+  let mut loop_stack: Vec<char> = Vec::new();
   for c in p.chars() {
     match c {
-      '>' => {}
-      '<' => {}
+      '>' => index_stack.push(c),
+      '<' => {
+        if let None = index_stack.pop() {
+          return Err(anyhow!("cannot index negative memory on tape"));
+        }
+      }
+      '[' => loop_stack.push(c),
+      ']' => {
+        if let None = loop_stack.pop() {
+          return Err(anyhow!("unbalanced number of brackets: too many ']'"));
+        }
+      }
+      // No special rules for these yet
       '+' => {}
       '-' => {}
       '.' => {}
       ',' => {}
-      '[' => {}
-      ']' => {}
-      c if c.is_whitespace() => {}
-      _ => {
-        return Err(InvalidProgram {
-          expected: "valid character",
-          found: "invalid character",
-        })
-      }
+
+      // Ignore all other characters since they are basically comments
+      _ => {}
     }
+  }
+  if loop_stack.len() != 0 {
+    Err(anyhow!("unbalanced number of brackets: too many '['"))
+  } else {
+    Ok(())
   }
 }
