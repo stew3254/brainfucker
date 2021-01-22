2937f119e3588df2ec62991440c659398b15b308
stew3254 <rtstewy@gmail.com>
2021-01-21 21:50:19 -0500

    Run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,140 +1,146 @@
-use anyhow::{anyhow, Error};
-use std::fs;
-use std::io::{stdin, Stdin, Read};
 use crate::parser::Command;
+use anyhow::{anyhow, Error};
 use std::collections::HashMap;
+use std::fs;
+use std::io::{stdin, Read, Stdin};
 
 mod parser;
 
 #[derive(Debug)]
 struct ProgramStack {
-  program: Vec<Command>,
-  inst_index: usize,
-  tape: HashMap<usize, u8>,
-  tape_index: usize,
-  stdin: Stdin,
+    program: Vec<Command>,
+    inst_index: usize,
+    tape: HashMap<usize, u8>,
+    tape_index: usize,
+    stdin: Stdin,
 }
 
 impl ProgramStack {
-  pub fn from(program: Vec<u8>) -> Result<ProgramStack, Error> {
-    if program.len() == 0 {
-      return Err(anyhow::anyhow!("Program cannot be of length 0"))
+    pub fn from(program: Vec<u8>) -> Result<ProgramStack, Error> {
+        if program.len() == 0 {
+            return Err(anyhow::anyhow!("Program cannot be of length 0"));
+        }
+
+        // See if the program is parsable
+        let p = match parser::parse(program) {
+            Ok(p) => p,
+            Err(e) => return Err(e),
+        };
+
+        Ok(ProgramStack {
+            program: p,
+            inst_index: 0,
+            tape: HashMap::new(),
+            tape_index: 0,
+            stdin: stdin(),
+        })
     }
-    
-    // See if the program is parsable
-    let p = match parser::parse(program) {
-      Ok(p) => p,
-      Err(e) => return Err(e),
-    };
 
-    Ok(ProgramStack {
-      program: p,
-      inst_index: 0,
-      tape: HashMap::new(),
-      tape_index: 0,
-      stdin: stdin(),
-    })
-  }
+    pub fn from_file(filename: String) -> Result<ProgramStack, Error> {
+        let program = match fs::read(filename) {
+            Ok(s) => s,
+            Err(e) => return Err(anyhow!(e)),
+        };
+        ProgramStack::from(program)
+    }
 
-  pub fn from_file(filename: String) -> Result<ProgramStack, Error> {
-    let program = match fs::read(filename) {
-      Ok(s) => s,
-      Err(e) => return Err(anyhow!(e)),
-    };
-    ProgramStack::from(program)
-  }
-  
-  pub fn run(&mut self) -> Result<(), Error> {
-    let len = self.program.len();
-    loop {
-      if self.inst_index >= len {
-        return Ok(())
-      }
-      match &self.program[self.inst_index] {
-        Command::IncrementPtr => {self.tape_index += 1;}
-        Command::DecrementPtr => {
-          // Make sure no negative bounds are indexed
-          if self.tape_index > 0 {
-            self.tape_index -= 1;
-          } else {
-            return Err(anyhow!("Tried to index a negative bound!"))
-          }
-        }
-        Command::IncrementValue => {
-          // Add value into tape if it's not 0 or change it if it's already there
-          match self.tape.get_mut(&self.tape_index) {
-            Some(b) => {*b = b.wrapping_add(1);}
-            None => {self.tape.insert(self.tape_index, 1);}
-          }
-        }
-        Command::DecrementValue => {
-          // Remove value into tape if it's going to be 0 or change it if it's already there
-          // or add it if it isn't
-          let mut remove_key = false;
-          match self.tape.get_mut(&self.tape_index) {
-            Some(b) => {
-              // Remove key since it would go to 0
-              if *b == 1 {
-                remove_key = true;
-              } else {
-                *b = b.wrapping_sub(1);
-              }
-            },
-            None => {self.tape.insert(self.tape_index, 255);}
-          }
-          if remove_key {
-            self.tape.remove(&self.tape_index);
-          }
-        }
-        Command::Output => {
-          print!(
-            "{}",
-            match self.tape.get(&self.tape_index) {
-              Some(b) => *b,
-              None => 0,
-            } as char
-          )
-        }
-        Command::Input => {
-          match self.stdin.lock().bytes().next() {
-            Some(b) =>  {
-              match b {
-                Ok(x) => self.tape.insert(self.tape_index, x),
-                Err(e) => return Err(anyhow!(e)),
-              };
+    pub fn run(&mut self) -> Result<(), Error> {
+        let len = self.program.len();
+        loop {
+            if self.inst_index >= len {
+                return Ok(());
             }
-            None => return Err(anyhow!("No input could be obtained")),
-          }
+            match &self.program[self.inst_index] {
+                Command::IncrementPtr => {
+                    self.tape_index += 1;
+                }
+                Command::DecrementPtr => {
+                    // Make sure no negative bounds are indexed
+                    if self.tape_index > 0 {
+                        self.tape_index -= 1;
+                    } else {
+                        return Err(anyhow!("Tried to index a negative bound!"));
+                    }
+                }
+                Command::IncrementValue => {
+                    // Add value into tape if it's not 0 or change it if it's already there
+                    match self.tape.get_mut(&self.tape_index) {
+                        Some(b) => {
+                            *b = b.wrapping_add(1);
+                        }
+                        None => {
+                            self.tape.insert(self.tape_index, 1);
+                        }
+                    }
+                }
+                Command::DecrementValue => {
+                    // Remove value into tape if it's going to be 0 or change it if it's already there
+                    // or add it if it isn't
+                    let mut remove_key = false;
+                    match self.tape.get_mut(&self.tape_index) {
+                        Some(b) => {
+                            // Remove key since it would go to 0
+                            if *b == 1 {
+                                remove_key = true;
+                            } else {
+                                *b = b.wrapping_sub(1);
+                            }
+                        }
+                        None => {
+                            self.tape.insert(self.tape_index, 255);
+                        }
+                    }
+                    if remove_key {
+                        self.tape.remove(&self.tape_index);
+                    }
+                }
+                Command::Output => {
+                    print!(
+                        "{}",
+                        match self.tape.get(&self.tape_index) {
+                            Some(b) => *b,
+                            None => 0,
+                        } as char
+                    )
+                }
+                Command::Input => match self.stdin.lock().bytes().next() {
+                    Some(b) => {
+                        match b {
+                            Ok(x) => self.tape.insert(self.tape_index, x),
+                            Err(e) => return Err(anyhow!(e)),
+                        };
+                    }
+                    None => return Err(anyhow!("No input could be obtained")),
+                },
+                Command::JumpForward(j) => {
+                    if let None = self.tape.get(&self.tape_index) {
+                        // Going to increase instruction pointer by 1 more after so this is fine
+                        self.inst_index = j.pair_index;
+                    }
+                }
+                Command::JumpBackward(j) => {
+                    if let Some(_) = self.tape.get(&self.tape_index) {
+                        // Going to increase instruction pointer by 1 more after so this is fine
+                        self.inst_index = j.pair_index;
+                    }
+                }
+            }
+            // Bump up instruction pointer by 1
+            self.inst_index += 1;
         }
-        Command::JumpForward(j) => {
-          if let None = self.tape.get(&self.tape_index) {
-            // Going to increase instruction pointer by 1 more after so this is fine
-            self.inst_index = j.pair_index;
-          }
-        }
-        Command::JumpBackward(j) => {
-          if let Some(_) = self.tape.get(&self.tape_index) {
-            // Going to increase instruction pointer by 1 more after so this is fine
-            self.inst_index = j.pair_index;
-          }
-        }
-      }
-      // Bump up instruction pointer by 1
-      self.inst_index += 1;
     }
-  }
-  
-  pub fn show_tape(&self) {
-    println!("{:?}", self.tape);
-  }
+
+    pub fn show_tape(&self) {
+        println!("{:?}", self.tape);
+    }
 }
 
 fn main() {
-  match ProgramStack::from_file(String::from("test.bf")) {
-    Ok(mut p) => {
-      p.run().unwrap();
-      p.show_tape();
-    },
-    Err(e) => println!("{}", e),
-  };
+    match ProgramStack::from_file(String::from("test.bf")) {
+        Ok(mut p) => {
+            p.run().unwrap();
+            p.show_tape();
+        }
+        Err(e) => println!("{}", e),
+    };
 }
--- src/parser.rs
+++ src/parser.rs
@@ -2,82 +2,78 @@ use anyhow::{anyhow, Error};
 
 #[derive(Debug)]
 pub struct Jump {
-  pub index: usize,
-  pub pair_index: usize,
+    pub index: usize,
+    pub pair_index: usize,
 }
 
 #[derive(Debug)]
 pub enum Command {
-  IncrementPtr,
-  DecrementPtr,
-  IncrementValue,
-  DecrementValue,
-  Output,
-  Input,
-  JumpForward(Jump),
-  JumpBackward(Jump),
+    IncrementPtr,
+    DecrementPtr,
+    IncrementValue,
+    DecrementValue,
+    Output,
+    Input,
+    JumpForward(Jump),
+    JumpBackward(Jump),
 }
 
 // Quickly parse program lint before trying to run
 pub fn parse(p: Vec<u8>) -> Result<Vec<Command>, Error> {
-  let mut program: Vec<Command> = Vec::new();
-  let mut jump_stack: Vec<usize> = Vec::new();
-  for c in p {
-    match c {
-      b'>' => program.push(Command::IncrementPtr),
-      b'<' => program.push(Command::DecrementPtr),
-      b'+' => program.push(Command::IncrementValue),
-      b'-' => program.push(Command::DecrementValue),
-      b'.' => program.push(Command::Output),
-      b',' => program.push(Command::Input),
-      b'[' => {
-        // Keep location in program saved for when we find a matching brace
-        jump_stack.push(program.len());
-        
-        // Add jump position to program
-        program.push(
-          Command::JumpForward(
-            // Set index of where we are in the program
-            Jump {
-              index: if program.len() > 0 {
-                program.len()-1
-              } else {
-                0
-              },
-              pair_index: 0,
+    let mut program: Vec<Command> = Vec::new();
+    let mut jump_stack: Vec<usize> = Vec::new();
+    for c in p {
+        match c {
+            b'>' => program.push(Command::IncrementPtr),
+            b'<' => program.push(Command::DecrementPtr),
+            b'+' => program.push(Command::IncrementValue),
+            b'-' => program.push(Command::DecrementValue),
+            b'.' => program.push(Command::Output),
+            b',' => program.push(Command::Input),
+            b'[' => {
+                // Keep location in program saved for when we find a matching brace
+                jump_stack.push(program.len());
+
+                // Add jump position to program
+                program.push(Command::JumpForward(
+                    // Set index of where we are in the program
+                    Jump {
+                        index: if program.len() > 0 {
+                            program.len() - 1
+                        } else {
+                            0
+                        },
+                        pair_index: 0,
+                    },
+                ));
             }
-          )
-        );
-      }
-      b']' => {
-        let len = program.len();
-        let mut pair_index: usize = 0;
-        match jump_stack.pop() {
-          Some(i) => {
-            if let Command::JumpForward(j)  = &mut program[i] {
-              j.pair_index = len;
-              pair_index = j.index;
-            }
-            program.push(
-              Command::JumpBackward(
-                // Set index of where we are in the program
-                Jump {
-                  index: len,
-                  pair_index,
+            b']' => {
+                let len = program.len();
+                let mut pair_index: usize = 0;
+                match jump_stack.pop() {
+                    Some(i) => {
+                        if let Command::JumpForward(j) = &mut program[i] {
+                            j.pair_index = len;
+                            pair_index = j.index;
+                        }
+                        program.push(Command::JumpBackward(
+                            // Set index of where we are in the program
+                            Jump {
+                                index: len,
+                                pair_index,
+                            },
+                        ));
+                    }
+                    None => return Err(anyhow!("unbalanced number of brackets: too many ']'")),
                 }
-              )
-            );
-          }
-          None => return Err(anyhow!("unbalanced number of brackets: too many ']'"))
+            }
+            // Ignore all other characters since they are basically comments
+            _ => {}
         }
-      }
-      // Ignore all other characters since they are basically comments
-      _ => {}
     }
-  }
-  if jump_stack.len() != 0 {
-    Err(anyhow!("unbalanced number of brackets: too many '['"))
-  } else {
-    Ok(program)
-  }
+    if jump_stack.len() != 0 {
+        Err(anyhow!("unbalanced number of brackets: too many '['"))
+    } else {
+        Ok(program)
+    }
 }
